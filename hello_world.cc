f1ead57ab258757cee577b892388ede01ddf52c8
ricklu <klu.china@google.com>
2013-04-30 09:42:30 +0800

    add hello-world program

---
+++ hello_world.cc
@@ -0,0 +1,11 @@
+// Copyright (C) 2013, FreeSoftware
+// Author: Ke Lu
+//
+// Description:
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    printf("hello world\n");
+    return 0;
+}
